9a57fa7c763ea4baf1e67f63b1815444da0c2629
Tuomas Vainio <tumaolva@student.jyu.fi>
2025-04-15 18:52:57 +0300

    testi

--- index.js
+++ index.js
@@ -12,46 +12,47 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary poppendieck",
-        number: "39-23-6423122"
-    }
-]
+// let persons = [
+//     {
+//         id: "1",
+//         name: "Arto Hellas",
+//         number: "040-123456"
+//     },
+//     {
+//         id: "2",
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523"
+//     },
+//     {
+//         id: "3",
+//         name: "Dan Abramov",
+//         number: "12-43-234345"
+//     },
+//     {
+//         id: "4",
+//         name: "Mary poppendieck",
+//         number: "39-23-6423122"
+//     }
+// ]
     app.get('/', (request, response) => {
      response.send('<h1>Hello World!</h1>')
      })
 
     app.get('/api/persons', (request, response) => {
         Person.find({}).then(result => {
-        response.json(persons)
+        response.json(people)
      })
     })
 
-     app.get('/api/persons/:id', (request, response) => {
-        const id = request.params.id;
-        const person = persons.find(person => person.id === id);
-
-      if (person) {
-        response.json(person)
-        } 
-      else {
+    app.get('/api/persons/:id', (request, response) => {
+        Person.findById(request.params.id).then(person => {
+            response.json(person.toJSON())
+        })
+        const id = request.params.id
+       if (id) {
+         response.json(person)
+         } 
+       else {
          response.status(404).end()
          console.log('Henkilöä ei ole olemassa')
         }
@@ -83,19 +84,21 @@ app.post('/api/persons', (request, response) => {
             error: 'Henkilö on olemassa'
         })
     }
-    const person = {
-        id: generateId(),
+    const person = new Person({
+       // id: generateId(),
         name: body.name,
-        number: body.number
-    }
-
+        number: body.number,
+    })
+    person.save().then(savedPerson => {
+        return savedPerson.toJSON()
+    })
     persons = persons.concat(person)
 
     response.json(person)
     
     console.log('henkilö lisätty')
 })
-     
+
 app.get('/info', (request, response) => {
     response.send(`<h2>Phonebook has info for ${persons.length} people</h2>
     <h2>${new Date()}</h2>`)
