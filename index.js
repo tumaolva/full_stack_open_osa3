8990d2a26e0631faeaff50fdb2860944e2b10088
Tuomas Vainio <tumaolva@student.jyu.fi>
2025-04-15 19:58:03 +0300

    3.15 testi renderillä

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express')
 const Person = require('./models/person')
 const app = express()
 const morgan = require('morgan')
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+
 
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
@@ -24,26 +25,27 @@ app.get('/api/persons', (request, response) => {
      })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-            response.json(person)
-    })
-    const id = request.params.id
-        if (id) {
-         response.json(person)
-        } 
-       else {
-         response.status(404).end()
-         console.log('Henkilöä ei ole olemassa')
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+       .then(person => {
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
         }
-})
+      })
+      .catch(error => next(error))
+  })
 
-app.delete('/api/persons/:id', (request, response) => {
-        const id = request.params.id
-        persons = persons.filter(person => person.id !== id)
-        response.status(204).end()
-        console.log('henkilö poistettu')
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      console.log('henkilö poistettu')
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+        
 
 // const generateId = () => {
 //     const maxId = Math.random() * 1000
@@ -84,6 +86,16 @@ app.get('/info', (request, response) => {
     <h2>${new Date()}</h2>`)
   })
 
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  app.use(errorHandler)
   const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
