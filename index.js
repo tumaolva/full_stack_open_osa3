c03fae430904d80409168cdbca75257f50ca8a9c
Tuomas Vainio <tumaolva@student.jyu.fi>
2025-04-15 18:55:22 +0300

    testi2

--- index.js
+++ index.js
@@ -12,35 +12,14 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// let persons = [
-//     {
-//         id: "1",
-//         name: "Arto Hellas",
-//         number: "040-123456"
-//     },
-//     {
-//         id: "2",
-//         name: "Ada Lovelace",
-//         number: "39-44-5323523"
-//     },
-//     {
-//         id: "3",
-//         name: "Dan Abramov",
-//         number: "12-43-234345"
-//     },
-//     {
-//         id: "4",
-//         name: "Mary poppendieck",
-//         number: "39-23-6423122"
-//     }
-// ]
+let persons = []
     app.get('/', (request, response) => {
      response.send('<h1>Hello World!</h1>')
      })
 
     app.get('/api/persons', (request, response) => {
         Person.find({}).then(result => {
-        response.json(people)
+        response.json(persons)
      })
     })
 
